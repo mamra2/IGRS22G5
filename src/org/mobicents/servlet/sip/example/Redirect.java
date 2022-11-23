9d60b5a73af2dc7c58de156d9768930797ffb1f7
Olek <roguezilla0@gmail.com>
2022-11-23 17:01:08 +0000

    Update Redirect.java

--- src/org/mobicents/servlet/sip/example/Redirect.java
+++ src/org/mobicents/servlet/sip/example/Redirect.java
@@ -1,171 +1,155 @@
-/*
- * $Id: EchoServlet.java,v 1.5 2003/06/22 12:32:15 fukuda Exp $
- */
 package org.mobicents.servlet.sip.example;
 
-import java.util.*;
-import java.io.IOException;
-
-import javax.servlet.sip.SipServlet;
-import javax.servlet.sip.SipServletRequest;
-import javax.servlet.sip.SipServletResponse;
 import javax.servlet.ServletException;
-import javax.servlet.sip.URI;
-import javax.sip.header.ContactHeader;
-import javax.servlet.sip.Proxy;
-import javax.servlet.sip.SipFactory;
+import javax.servlet.sip.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-/**
- */
 public class Redirect extends SipServlet {
+    static private Map<String, String> registrarDB;
+    static private SipFactory sipFactory;
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	static private Map<String, String> RegistrarDB; // The Location Database 
-	static private SipFactory factory;              // Factory for creating Req.s and URIs
-	
-	public Redirect() {
-		super();
-	}
-	
-	public void init() {
-		factory = (SipFactory) getServletContext().getAttribute(SIP_FACTORY);
-		RegistrarDB = new HashMap<String,String>();
-	}
-	
-	protected void send403(SipServletRequest request) throws ServletException,
-	IOException {
-		SipServletResponse response = request.createResponse(403);
-		response.send();
-	}
-	
-	protected void send200(SipServletRequest request) throws ServletException,
-	IOException {
-		SipServletResponse response = request.createResponse(202);
-		response.send();
-	}
-	
-	protected boolean verifyDomain(SipServletRequest request) throws ServletException,
-	IOException {
-		String aor = getAttr(request.getHeader("To"), "@");
-		log("========================" + aor);
-		return aor.compareTo("@acme.pt")==0;
-	}
-	
-	protected boolean isColaborator(SipServletRequest request) throws ServletException,
-	IOException {
-		return getAttr(request.getHeader("To"), "sip:").contains("colaborador");
-	}
-	
-	protected boolean isGestor(SipServletRequest request) throws ServletException,
-	IOException {
-		return getAttr(request.getHeader("To"), "sip:").contains("gestor");
-	}
+    /**
+     * SipServlet functions
+     */
+    @Override
+    public void init() {
+        sipFactory = (SipFactory) getServletContext().getAttribute(SIP_FACTORY);
+        registrarDB = new HashMap<>();
+    }
 
-	/**
-        * Acts as a registrar and location service for REGISTER messages
-        * @param  request The SIP message received by the AS 
-        */
-	protected void doRegister(SipServletRequest request) throws ServletException,
-			IOException {
-		
-		if(!verifyDomain(request)){
-			send403(request);
-			return;
-		
-		}
-		
-		String toHeader = request.getHeader("To");
-		String contactHeader = request.getHeader("Contact");
-		String aor = getAttr(toHeader, "sip:");	
-		String contact = getAttr(contactHeader, "sip:");
-		RegistrarDB.put(aor, contact);
-		
-		SipServletResponse response; 
-		response = request.createResponse(200);
-		response.send();
-		
-	    // Some logs to show the content of the RegistrarDB database.
-		log("REGISTER:******");
-		Iterator<Map.Entry<String,String>> it = RegistrarDB.entrySet().iterator();
-    		while (it.hasNext()) {
-        		Map.Entry<String,String> pairs = (Map.Entry<String,String>)it.next();
-        		log(pairs.getKey() + " = " + pairs.getValue());
-    		}
-		log("REGISTER:******");
-		log("======" + contactHeader.split("=")[1]);
-	}
+    @Override
+    protected void doRegister(SipServletRequest request) throws IOException {
+        if (!verifyDomain(request)) {
+            request.createResponse(403).send();
+            return;
+        }
 
-	/**
-        * Sends SIP replies to INVITE messages
-        * - 300 if registred
-        * - 404 if not registred
-        * @param  request The SIP message received by the AS 
-        */
-	protected void doInvite(SipServletRequest request)
-                  throws ServletException, IOException {
-		
-		// Some logs to show the content of the Registrar database.
-		log("INVITE:***");
-		Iterator<Map.Entry<String,String>> it = RegistrarDB.entrySet().iterator();
-    		while (it.hasNext()) {
-        		Map.Entry<String,String> pairs = (Map.Entry<String,String>)it.next();
-        		log(pairs.getKey() + " = " + pairs.getValue());
-    		}
-		log("INVITE:***");
-		
-		String aor = getAttr(request.getHeader("To"), "sip:"); // Get the To AoR
-		log("INVITE: To: " + aor);
-	    if (!RegistrarDB.containsKey(aor)) { // To AoR not in the database, reply 404
-			
-	    	request.createResponse(100).send();
-			Proxy proxy = request.getProxy();
-			ArrayList<URI> addrList = new ArrayList<URI>();
-			addrList.add(factory.createURI(aor));
-			proxy.proxyTo(addrList);
-			log("PROXYYYYYYY");
-	    	
-	    	
-	    	/*
-	    	SipServletResponse response; 
-			response = request.createResponse(404);
-			response.send();
-			*/	
-			
-	    } else {
-			// SipServletRequest req = factory.createRequest(request.getApplicationSession(), "INFO", "sip:server@acme.pt", RegistrarDB.get(aor));
-	    	
-	    	SipServletResponse response = request.createResponse(300);
-			// Get the To AoR contact from the database and add it to the response 
-			response.setHeader("Contact",RegistrarDB.get(aor));
-			response.send();
-			
-		}
-	}
-	
-	protected void doResponse(SipServletResponse response)
-            throws ServletException, IOException {
-		log("===!==!=!=!=!=!==!=!=!=!=!="+response.getMethod());
-	}
-	
-	
-	/**
-        * Auxiliary function for extracting attribute values
-        * @param str the complete string
-        * @param attr the attr name 
-        * @return attr name and value 
-        */
-	protected String getAttr(String str, String attr) {
-		int indexStart = str.indexOf(attr);
-		int indexStop  = str.indexOf(">", indexStart);
-		if (indexStop == -1) {
-			indexStop  = str.indexOf(";", indexStart);
-			if (indexStop == -1) {
-				indexStop = str.length();
-			}
-		}
-		return str.substring(indexStart, indexStop);
-	}
+        if (verifyUserType(request, "colaborador")) {
+            log("COLABORADOR");
+        } else if (verifyUserType(request, "gestor")) {
+            log("GESTOR");
+        } else {
+            log("?????");
+        }
+
+        String toHeader = request.getHeader("To");
+        String contactHeader = request.getHeader("Contact");
+        String aor = getAttr(toHeader, "sip:");
+        String contact = getAttr(contactHeader, "sip:");
+
+        // if not null then linphone
+        String expires = request.getHeader("Expires");
+        if (expires != null) {
+            // linphone sends 2 registers for no reason, the one with @127.0.0.1:5666 is to be ignored
+            if (getAttr(contactHeader, "@").equals("@127.0.0.1:5666")) {
+                request.createResponse(200).send();
+                return;
+            }
+
+            if (expires.equals("0")) {
+                if (!registrarDB.containsKey(aor)) {
+                    request.createResponse(404).send();
+                } else {
+                    registrarDB.remove(aor);
+                    request.createResponse(200).send();
+                }
+            } else {
+                log("LINPHONE REGISTER");
+
+                if (registrarDB.containsKey(aor)) {
+                    request.createResponse(409).send();
+                    return;
+                }
+
+                registrarDB.put(aor, contact);
+                registrarDB.put("alerta@acme.pt", registrarDB.get(aor));
+                request.createResponse(200).send();
+            }
+        } else {
+            if (contactHeader.split("=")[1].equals("0")) {
+                log("TWINKLE UNREGISTER");
+
+                if (!registrarDB.containsKey(aor)) {
+                    request.createResponse(404).send();
+                } else {
+                    registrarDB.remove(aor);
+                    request.createResponse(200).send();
+                }
+            } else {
+                log("TWINKLE REGISTER");
+
+                if (registrarDB.containsKey(aor)) {
+                    request.createResponse(409).send();
+                    return;
+                }
+
+                registrarDB.put(aor, contact);
+                request.createResponse(200).send();
+            }
+        }
+    }
+
+    @Override
+    protected void doInvite(SipServletRequest request) throws IOException, TooManyHopsException, ServletParseException {
+        if (!verifyDomain(request)) {
+            request.createResponse(403).send();
+            return;
+        }
+
+        String aor = getAttr(request.getHeader("To"), "sip:");
+
+        if (!registrarDB.containsKey(aor)) {
+            log("PROXY");
+            request.createResponse(404).send();
+        } else {
+            SipServletResponse response = request.createResponse(300);
+            response.setHeader("Contact", registrarDB.get(aor));
+            response.send();
+        }
+    }
+
+    @Override
+    protected void doMessage(SipServletRequest request) throws ServletException, IOException {
+        String aor = getAttr(request.getHeader("To"), "sip:");
+
+        if (aor.equals("alerta@acme.pt")) {
+            request.createResponse(200).send();
+            SipServletResponse response = request.createResponse(300);
+            response.setHeader("To", "<sip:gestor@acme.pt>");
+            response.setHeader("Contact", registrarDB.get("gestor@acme.pt"));
+            response.send();
+        }
+    }
+
+    /**
+     * Customs functions
+     */
+    protected boolean verifyDomain(SipServletRequest request) {
+        return getAttr(request.getHeader("From"), "@").equals("@acme.pt");
+    }
+
+    protected boolean verifyUserType(SipServletRequest request, String type) {
+        return getAttr(request.getHeader("To"), "sip:").contains(type);
+    }
+
+    /**
+     * Auxiliary function for extracting attribute values
+     *
+     * @param str  the complete string
+     * @param attr the attr name
+     * @return attr name and value
+     */
+    protected String getAttr(String str, String attr) {
+        int indexStart = str.indexOf(attr);
+        int indexStop = str.indexOf(">", indexStart);
+        if (indexStop == -1) {
+            indexStop = str.indexOf(";", indexStart);
+            if (indexStop == -1) {
+                indexStop = str.length();
+            }
+        }
+        return str.substring(indexStart, indexStop);
+    }
 }
