4187a4511dbf89546cb5b3dcd2d03d3ff37ef78f
Olek <roguezilla0@gmail.com>
2022-11-24 15:24:48 +0000

    intellij project structure

---
+++ src/Redirect.java
@@ -0,0 +1,110 @@
+import javax.servlet.ServletException;
+import javax.servlet.sip.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Redirect extends SipServlet {
+    static private Map<String, String> registrarDB;
+    static private SipFactory sipFactory;
+
+    /**
+     * SipServlet functions
+     */
+    @Override
+    public void init() {
+        sipFactory = (SipFactory) getServletContext().getAttribute(SIP_FACTORY);
+        registrarDB = new HashMap<>();
+    }
+
+    @Override
+    protected void doRegister(SipServletRequest request) throws IOException {
+        if (!verifyDomain(request)) {
+            request.createResponse(403).send();
+            return;
+        }
+
+        if (!(verifyUserType(request, "colaborador") || verifyUserType(request, "gestor"))) {
+            request.createResponse(403).send();
+            return;
+        }
+
+        String contactHeader = request.getHeader("Contact");
+        String aor = getAttr(request.getHeader("To"), "sip:");
+        String contact = getAttr(contactHeader, "sip:");
+
+        // if not null then linphone
+        String expiresHeader = request.getHeader("Expires");
+        String expiry = expiresHeader != null ? expiresHeader : contactHeader.split("=")[1];
+        System.out.println(expiresHeader != null ? "LINPHONE" : "TWINKLE");
+        if (expiry.equals("0")) {
+            log("SIP UNREGISTER");
+
+            if (!registrarDB.containsKey(aor)) {
+                request.createResponse(404).send();
+            } else {
+                registrarDB.remove(aor);
+                request.createResponse(200).send();
+            }
+        } else {
+            log("SIP REGISTER");
+
+            registrarDB.put(aor, contact);
+            request.createResponse(200).send();
+        }
+    }
+
+    @Override
+    protected void doInvite(SipServletRequest request) throws IOException, TooManyHopsException, ServletParseException {
+        if (!verifyDomain(request)) {
+            request.createResponse(403).send();
+            return;
+        }
+
+        String aor = getAttr(request.getHeader("To"), "sip:");
+
+        if (!registrarDB.containsKey(aor)) {
+            log("PROXY FORBIDDEN");
+            request.createResponse(404).send();
+        } else {
+            SipServletResponse response = request.createResponse(300);
+            response.setHeader("Contact", registrarDB.get(aor));
+            response.send();
+        }
+    }
+
+    @Override
+    protected void doMessage(SipServletRequest request) throws ServletException, IOException {
+        String aor = getAttr(request.getHeader("To"), "sip:");
+    }
+
+    /**
+     * Customs functions
+     */
+    protected boolean verifyDomain(SipServletRequest request) {
+        return getAttr(request.getHeader("From"), "@").equals("@acme.pt");
+    }
+
+    protected boolean verifyUserType(SipServletRequest request, String type) {
+        return getAttr(request.getHeader("To"), "sip:").contains(type);
+    }
+
+    /**
+     * Auxiliary function for extracting attribute values
+     *
+     * @param str  the complete string
+     * @param attr the attr name
+     * @return attr name and value
+     */
+    protected String getAttr(String str, String attr) {
+        int indexStart = str.indexOf(attr);
+        int indexStop = str.indexOf(">", indexStart);
+        if (indexStop == -1) {
+            indexStop = str.indexOf(";", indexStart);
+            if (indexStop == -1) {
+                indexStop = str.length();
+            }
+        }
+        return str.substring(indexStart, indexStop);
+    }
+}
