7604c297b99d1695b528bc4f44ebb6a651c54502
Olek <roguezilla0@gmail.com>
2022-11-25 19:41:27 +0000

    colaborator handling via messages

--- src/Redirect.java
+++ src/Redirect.java
@@ -7,6 +7,7 @@ import java.util.*;
 public class Redirect extends SipServlet {
     static private Map<String, String> registrarDB;
     static private Map<String, Boolean> stateDB;
+    static private ArrayList<String> colabDB;
     static private SipFactory sipFactory;
 
     /**
@@ -18,13 +19,14 @@ public class Redirect extends SipServlet {
 
         registrarDB = new HashMap<>();
         stateDB = new HashMap<>();
+        colabDB = new ArrayList<>();
     }
 
     @Override
     protected void doPublish(SipServletRequest request) throws ServletException, IOException {
         String aor = getAttr(request.getHeader("From"), "sip:");
 
-        if (!verifyDomain(request) || !(verifyUserType(request, "colaborador") || verifyUserType(request, "gestor"))) {
+        if (!inDomain(request) || !(userType(request, "colaborador") || userType(request, "gestor"))) {
             request.createResponse(403).send();
             return;
         }
@@ -34,14 +36,13 @@ public class Redirect extends SipServlet {
             return;
         }
 
-        // this has an Expires header, so handle that maybe?
         stateDB.put(aor, request.getHeader("Content-Length").equals("194"));
         request.createResponse(200).send();
     }
 
     @Override
     protected void doRegister(SipServletRequest request) throws IOException {
-        if (!verifyDomain(request) || !(verifyUserType(request, "colaborador") || verifyUserType(request, "gestor"))) {
+        if (!inDomain(request) || !(userType(request, "colaborador") || userType(request, "gestor"))) {
             request.createResponse(403).send();
             return;
         }
@@ -75,50 +76,93 @@ public class Redirect extends SipServlet {
 
     @Override
     protected void doInvite(SipServletRequest request) throws IOException, TooManyHopsException, ServletParseException {
-        if (!verifyDomain(request)) {
+        if (!inDomain(request)) {
             request.createResponse(403).send();
             return;
         }
 
-        String aor = getAttr(request.getHeader("To"), "sip:");
+        String aorTo = getAttr(request.getHeader("To"), "sip:");
 
-        if (!registrarDB.containsKey(aor)) {
-            log("PROXY FORBIDDEN");
-            request.createResponse(404).send();
+        if (!registrarDB.containsKey(aorTo)) {
+            if (userType(request, "gestor") && aorTo.equals("sip:alerta@acme.pt")) {
+                SipServletRequest customRequest = sipFactory.createRequest(request.getApplicationSession(), "MESSAGE", "sip:alerta@acme.pt", registrarDB.get("sip:gestor@acme.pt"));
+                customRequest.setContent("Consola\nADD address\nREMOVE address".getBytes(), "text/plain");
+                customRequest.send();
+                request.createResponse(200).send();
+            } else {
+                request.createResponse(404).send();
+            }
         } else {
-            request.getProxy().proxyTo(sipFactory.createURI(registrarDB.get(aor)));
+            request.getProxy().proxyTo(sipFactory.createURI(registrarDB.get(aorTo)));
         }
     }
 
     @Override
-    protected void doMessage(SipServletRequest request) throws ServletException, IOException {
-        String aor = getAttr(request.getHeader("To"), "sip:");
+    protected void doBye(SipServletRequest request) throws ServletException, IOException {
+        request.createResponse(200).send();
+    }
 
-        if (aor.equals("sip:alerta@acme.pt")) {
-            if (registrarDB.containsKey("sip:gestor@acme.pt")) {
-                log("PROXY TO MANAGER");
-                request.getProxy().proxyTo(sipFactory.createURI(registrarDB.get("sip:gestor@acme.pt")));
-                log("MESSAGE CONTENT LEN: " + request.getContentLength());
-                log("MESSAGE CONTENT: " + new String(request.getRawContent(), StandardCharsets.UTF_8));
-                request.createResponse(200).send();
-            }
+    @Override
+    protected void doMessage(SipServletRequest request) throws ServletException, IOException {
+        String aorFrom = getAttr(request.getHeader("From"), "sip:");
+        String aorTo = getAttr(request.getHeader("To"), "sip:");
+
+        if (userType(request, "colaborador") && aorTo.equals("sip:alerta@acme.pt") && registrarDB.containsKey("sip:gestor@acme.pt")) {
+            request.getProxy().proxyTo(sipFactory.createURI(registrarDB.get("sip:gestor@acme.pt")));
+            request.createResponse(200).send();
+            return;
         }
 
-        if (aor.equals("sip:gestor@acme.pt")) {
-            log("PROXY WORKED");
+        if (userType(request, "gestor") && aorTo.equals("sip:alerta@acme.pt") ) {
+            // 0 - command
+            // 1 - argument
+            String[] msg = new String(request.getRawContent(), StandardCharsets.UTF_8).split(" ");
+            switch (msg[0]) {
+                case "ADD": {
+                    log("==========================ADD");
+                    if (!registrarDB.containsKey(msg[1])) {
+                        request.createResponse(404).send();
+                        return;
+                    }
+                    colabDB.add(msg[1]);
+                    SipServletRequest res = sipFactory.createRequest(request.getApplicationSession(), "MESSAGE", "sip:alerta@acme.pt", registrarDB.get(aorFrom));
+                    res.setContent("OK".getBytes(), "text/plain");
+                    res.send();
+                    request.createResponse(200).send();
+                    break;
+                }
+                case "REMOVE": {
+                    log("==========================REMOVE");
+                    if (!registrarDB.containsKey(msg[1])) {
+                        request.createResponse(404).send();
+                        return;
+                    }
+                    colabDB.remove(msg[1]);
+                    request.createResponse(200).send();
+                    break;
+                }
+                default:
+                    request.createResponse(403).send();
+            }
+            return;
+        }
+
+        if (userType(request, "colaborador")) {
+            request.getProxy().proxyTo(sipFactory.createURI(registrarDB.get(aorTo)));
             request.createResponse(200).send();
+            return;
         }
     }
 
     /**
      * Customs functions
      */
-    protected boolean verifyDomain(SipServletRequest request) {
+    protected boolean inDomain(SipServletRequest request) {
         return getAttr(request.getHeader("From"), "@").equals("@acme.pt");
     }
 
-    protected boolean verifyUserType(SipServletRequest request, String type) {
-        return getAttr(request.getHeader("To"), "sip:").contains(type);
+    protected boolean userType(SipServletRequest request, String type) {
+        return getAttr(request.getHeader("From"), "sip:").contains(type);
     }
 
     /**
